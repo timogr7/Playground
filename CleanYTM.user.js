a0a709ac2a082478b069687f832f03c7272ae9ab
timogr7 <72560284+timogr7@users.noreply.github.com>
2022-09-25 22:49:27 +0200

    Create CleanYTM.user.js

---
+++ CleanYTM.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+ // @name         Hide YouTube Recommandations In Home Page
+ // @namespace    http://tampermonkey.net/
+ // @version      0.2
+ // @description  Time is Precious
+ // @author       You
+ // @match        *://www.youtube.com/
+ // @match        *://www.youtube.com/?*
+ // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+ // @grant        none
+ // @license      MIT
+ // ==/UserScript==
+
+ (function() {
+     'use strict';
+
+     const newStyle = document.createElement("style")
+     newStyle.innerHTML = `
+         #related,
+ ytd-section-list-renderer[page-subtype="home"],
+ #masthead-ad,
+ ytd-rich-grid-renderer,
+ ytd-mealbar-promo-renderer
+ {
+     display:none !important;
+ }
+ ytd-browse[page-subtype="home"] {
+     background-color:gray;
+     /* here is my YT background wallpaper, leave it or use a different url */
+     background-image:url('https://wallpaperaccess.com/full/2923100.jpg');
+     background-size:cover;
+ 	background-attachment:fixed;
+     background-position:50% 50%;
+ 	min-height:90vh;
+ }
+ #content {
+ 	min-height:100%;
+ }
+     }
+     `
+     document.head.appendChild(newStyle)
+     // Your code here...
+ })();

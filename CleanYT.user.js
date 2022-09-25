d1092ed12777407b97b3f5ace6f6493a379a5649
timogr7 <72560284+timogr7@users.noreply.github.com>
2022-09-25 22:30:54 +0200

    Update CleanYT.user.js

--- CleanYT.user.js
+++ CleanYT.user.js
@@ -33,6 +33,22 @@ ytd-browse[page-subtype="home"] {
     background-position:50% 50%;
 	min-height:90vh;
 }
+ytm-section-list-renderer[page-subtype="home"],
+#masthead-ad,
+ytm-rich-grid-renderer,
+ytm-mealbar-promo-renderer
+{
+    display:none !important;
+}
+ytm-browse[page-subtype="home"] {
+    background-color:gray;
+    /* here is my YT background wallpaper, leave it or use a different url */
+    background-image:url('https://wallpaperaccess.com/full/2923100.jpg');
+    background-size:cover;
+	background-attachment:fixed;
+    background-position:50% 50%;
+	min-height:90vh;
+}
 
 #content {
 	min-height:100%;

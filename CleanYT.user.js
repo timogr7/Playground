22966322b645d77228057ee0e5897c603fa9bf29
timogr7 <72560284+timogr7@users.noreply.github.com>
2022-09-25 22:20:34 +0200

    Update CleanYT.user.js

--- CleanYT.user.js
+++ CleanYT.user.js
@@ -4,8 +4,8 @@
 // @version      0.2
 // @description  Time is Precious
 // @author       You
-// @match        *://www.youtube.com/
-// @match        *://www.youtube.com/?*
+// @match        *.youtube.com/
+// @match        *.youtube.com/?*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // @license      MIT
